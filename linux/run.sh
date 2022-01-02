3ff411a270af37c7407836f07d48093551876d68
tygr <workfile975@gmail.com>
2022-01-02 15:40:45 +0800

    install fish shell

--- linux/run.sh
+++ linux/run.sh
@@ -6,7 +6,8 @@
 sudo apt-get update -y
 
 # basic
-sudo apt-get install build-essential
+sudo apt-get install build-essential -y
+sudo apt-get install curl -y
 
 ## server manage
 sudo apt-get install openssh-server -y
@@ -17,6 +18,10 @@ sudo apt-get install tree -y
 cp ~/.bashrc ~/.bashrc_bg2
 sed -i -f bashrc.sedCmd ~/.bashrc
 source ~/.bashrc
+
+## finsh shell
+sudo apt-get install fish -y
+chsh -s /usr/bin/fish
 
 ## editor 
 sudo apt-get install vim -y
